c05f159baac7397c7120b7ea0d22043f31094ad3
RadekRo <r.roclawski@ankara.pl>
2024-01-29 19:57:29 +0100

    tests: implemented basic tests for clientsController

---
+++ ClientsControllerTests.cs
@@ -0,0 +1,42 @@
+﻿using Moq;
+using AnkaraLab_BackEnd.WebAPI.Controllers;
+using AnkaraLab_BackEnd.WebAPI.Infrastructure.Interfaces;
+using Microsoft.Extensions.Logging;
+using Microsoft.AspNetCore.Mvc;
+using AutoMapper;
+
+
+namespace AnkaraLab_Backend.Tests
+{
+    public class ClientsControllerTests
+    {
+        [Test]
+        public async Task GetClients_ReturnsOkResult()
+        {
+            var mockRepository = new Mock<IClientRepository>();
+            var mockMapper = new Mock<IMapper>();
+            var mockLogger = new Mock<ILogger<ClientsController>>();
+            var controller = new ClientsController(mockRepository.Object, mockMapper.Object, mockLogger.Object);
+
+            var result = await controller.GetClients();
+
+            Assert.IsInstanceOf<OkObjectResult>(result.Result);
+            Assert.IsNotNull((result.Result as OkObjectResult)?.Value);
+        }
+
+        [Test]
+        public async Task GetClient_ReturnsOkResultForValidId()
+        {
+            var mockRepository = new Mock<IClientRepository>();
+            var mockMapper = new Mock<IMapper>();
+            var mockLogger = new Mock<ILogger<ClientsController>>();
+            var controller = new ClientsController(mockRepository.Object, mockMapper.Object, mockLogger.Object);
+
+            var result = await controller.GetClient(1);
+
+            Assert.IsInstanceOf<OkObjectResult>(result.Result);
+            Assert.IsNotNull((result.Result as OkObjectResult)?.Value);
+        }
+
+    }
+}
