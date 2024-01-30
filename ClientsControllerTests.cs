cbf519bfe6771dd2a31007bb5a4a91fd4081354a
RadekRo <r.roclawski@ankara.pl>
2024-01-30 22:41:24 +0100

    fix: corrected the test for clients controller due to wrong object result. ClientDto -> Client

--- ClientsControllerTests.cs
+++ ClientsControllerTests.cs
@@ -4,6 +4,7 @@ using AnkaraLab_BackEnd.WebAPI.Infrastructure.Interfaces;
 using Microsoft.Extensions.Logging;
 using Microsoft.AspNetCore.Mvc;
 using AutoMapper;
+using AnkaraLab_BackEnd.WebAPI.Domain;
 
 
 namespace AnkaraLab_Backend.Tests
@@ -30,12 +31,37 @@ namespace AnkaraLab_Backend.Tests
             var mockRepository = new Mock<IClientRepository>();
             var mockMapper = new Mock<IMapper>();
             var mockLogger = new Mock<ILogger<ClientsController>>();
+
+            mockRepository.Setup(repo => repo.GetClientAsync(It.IsAny<int>()))
+                            .ReturnsAsync(new Client
+                            {
+                              Id = 1,
+                              Name = "Jarosław",
+                              Surname = "Kaczyński",
+                              Discount = 0,
+                              LastLoginDate = DateTime.Now,
+                              Email = "",
+                              Password = "",
+                              Phone = "",
+                              Newsletter = false,
+                              IsActive = true,
+                              IsAdmin = false,
+                              Status = 1,
+                              FtpLogin = "",
+                              FtpPassword = "",
+                              Epoint = 0,
+                              Deposit = 0,
+                              Deadline = DateTime.Now
+                          });
+
             var controller = new ClientsController(mockRepository.Object, mockMapper.Object, mockLogger.Object);
 
             var result = await controller.GetClient(1);
 
             Assert.IsInstanceOf<OkObjectResult>(result.Result);
             Assert.IsNotNull((result.Result as OkObjectResult)?.Value);
+
+            mockRepository.Verify(repo => repo.GetClientAsync(1), Times.Once, "OK");
         }
 
     }
